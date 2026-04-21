6f780972fbe0027849e3f4a4c0249b9733aad017
Reen mit <lovleyreen@gmail.com>
2026-04-21 18:21:44 +0300

    Update health app messages for age categories
    Added health app code for age-based messages.

---
+++ health _app.py
@@ -0,0 +1,10 @@
+print("Welcome to my Health App")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age < 18:
+   print(name, "you are a minor. Focus on good nutrition and exercise.")
+else:
+print(name, "you are an adult. Maintain a healthy lifestyle.")
+    added health app code
